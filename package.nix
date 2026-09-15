27c04f59ddf1845fb6040e4496b9817a8ff79d40
Astronot233 <tyz.err.233@gmail.com>
2026-09-15 19:10:38 +0800

    v0.70.0

--- package.nix
+++ package.nix
@@ -6,18 +6,18 @@
 }:
 stdenv.mkDerivation rec {
   pname = "xmcl";
-  version = "0.68.0";
+  version = "0.70.0";
   src = (
     let
       base = "https://github.com/Voxelum/x-minecraft-launcher/releases/download/v${version}";
       gzs = {
         x86_64-linux = {
           url = "${base}/app-${version}-linux.asar.gz";
-          hash = "sha256:1d9e5149698ff9df19596d8e10eccba9c834ba419f7c34dc383d1c0a144cc68c";
+          hash = "sha256:a63fd73ca81631a3f59ae4d274cee2583f32039145bae7fec186c2cf8497a81e";
         };
         aarch64-linux = {
           url = "${base}/app-${version}-linux-arm64.asar.gz";
-          hash = "sha256:cab25924976eeaa57c54a9785efd7ed953c8804a26b997e10080cb666cefe68c";
+          hash = "sha256:90bc8a1b5293549cfbd1e968379a49f4eeee198c5527e9adfc540aedf5bbcb98";
         };
       };
       sys = stdenv.hostPlatform.system;
